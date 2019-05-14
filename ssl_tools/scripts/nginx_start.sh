8322975130661c3a7748b24f235297e7674d300d
Alexey Evseev @stalk <myhappydo@gmail.com>
2019-05-14 11:40:09 +0300

    Add nginx config with unmanaged domain

--- ssl_tools/scripts/nginx_start.sh
+++ ssl_tools/scripts/nginx_start.sh
@@ -5,12 +5,18 @@ NGINX=/etc/init.d/nginx
 NGINX_OUTPUT_PATH=${SCRIPT_PATH}/../output/nginx
 
 mkdir -p ${NGINX_OUTPUT_PATH}
-# Copy, but don't overwrite (-n option)
-cp -n ${SCRIPT_PATH}/../nginx/sites-available/acme_example.source ${NGINX_OUTPUT_PATH}/acme_example
+for file_path in ${SCRIPT_PATH}/../nginx/sites-available/*.source;
+do
+    # Copy all *.source files to output folder
+    file_name=$(basename ${file_path})
+    result_file_name=$(echo ${file_name} | cut -d. -f1)
+    # Copy, but don't overwrite (-n option)
+    cp -n ${file_path} ${NGINX_OUTPUT_PATH}/${result_file_name}
+    # create or update symlinks
+    ln -sf ${NGINX_OUTPUT_PATH}/${result_file_name} ${NGINX_PATH}/sites-available/${result_file_name}
+    ln -sf ${NGINX_PATH}/sites-available/${result_file_name} ${NGINX_PATH}/sites-enabled/${result_file_name}
 
-# create or update symlinks
-ln -sf ${NGINX_OUTPUT_PATH}/acme_example ${NGINX_PATH}/sites-available/acme_example
-ln -sf ${NGINX_PATH}/sites-available/acme_example ${NGINX_PATH}/sites-enabled/acme_example
+done
 
 ${NGINX} start
 ${NGINX} reload
